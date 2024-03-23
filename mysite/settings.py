2644b7c145cd136fcc4c5176289963d797c0833b
Brenda Roncal <brendaroncal@MacBook-Air-di-Brenda.local>
2024-03-23 12:14:08 +0100

    add subdomain to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-62b!szb6r-l5b01ltaz$#z3imsk15)g4nd(dw(l_2gc*5e_g%p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '127.0.0.1', 'pythonanywhere.com' ]
+ALLOWED_HOSTS = [ '127.0.0.1', 'broncal.pythonanywhere.com' ]
 
 
 # Application definition
